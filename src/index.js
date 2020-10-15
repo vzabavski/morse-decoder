dcf052364d6c6f6c7e169535f196d841773e0bf6
vzabavski <vzabavski123@gmail.com>
2020-10-15 20:19:45 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,30 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let decoded = ''
+  let arr = [];
+  let letter = '';
+
+  while (expr.length > 0) {
+    arr.push(expr.substr(0, 10));
+    expr = expr.slice(10, expr.length);
+  }
+  for (let a of arr) {
+    if (a === '**********') {
+      decoded += ' ';
+    } else {
+      for (let i = 0; i < a.length; i +=2) {
+        if (a[i] === '1' && a[i + 1] === '0') {
+          letter += '.'
+        } else if (a[i] === '1' && a[i + 1] === '1') {
+          letter += '-'
+        }
+      }
+       decoded += MORSE_TABLE[letter]
+       letter = '';
+    }
+  }
+  return  decoded
 }
 
 module.exports = {
